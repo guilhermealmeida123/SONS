ac1f71f337355d76c705ad80a3c19079e21aa34b
guilhermealmeida123 <105080670+guilhermealmeida123@users.noreply.github.com>
2023-11-24 11:21:33 -0300

    ok

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
-//Declaração/criação da função
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-}
+//Declaraão/criação da função tocaSomPom
 
-const ListaDeTeclas = document.querySelectorAll('.tecla');
+function tocaSom(ideElementoAudio) (
 
+    document.querySelector(ideElementoAudio).play();
+    //criando a referencias constante listaDeTeclas e buscando a classe coletiva tecl const listaDeTeclas = document.querySelectorAll('.tecla'); 
+     //ceiação da repetição "while" e trocado pelo "for" com variavel, condição e increm for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+        const teclas listaDeTeclas [contador]; //guarda o contador da lista const efeito teclas.classList[1]; //chama o 2 item classe individual do som const idAudio #som Sefeito); //utiliza a junção template string ${}
 
-for(let contador = 0;contador < ListaDeTeclas.length; contador = contador + 1){
-    const teclas = ListaDeTeclas [contador]
-    const efeito = teclas.classList[1];
-    const idAudio = `#som_${efeito}`;
-    teclas.onclick = function(){
-        tocaSom(idAudio);
-    }
-    teclas.onkeydown = function(){
-        teclas.classList.add('ativa');
-    }
-    {
-        teclas.onkeyup = function(){
-            teclas.classList.remove('ativa');
-        }
-    }
- console.log(contador);   
-}
+tocasom(idAudio):
+
+teclas.onclick = function(){ //função anônima auxiliar para chamar tocasom //chamada da função tocasom com parâmetro idAudio
+
+teclas.onkeydown = function(evento) {
+
+console.log(evento.code == 'Space')
+
+if (evento.code == 'Space')( if(evento.code != 'Tab'){ teclas.classList.add('ativa');
+
+teclas.onkeyup=function(){ teclas.classList.remove('ativa');
