e8a924559683a2d2e0e3608e3e6206eed87c508f
guilhermealmeida123 <105080670+guilhermealmeida123@users.noreply.github.com>
2023-12-01 11:10:30 -0300

    ok

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-//Declaraão/criação da função tocaSomPom
+//DECLARAÇÃO/criação da função tocaSomPom
+function tocaSomPom(idElementoAudio){                   
+    document.querySelector(idElementoAudio).play();
+}
+//criando a referêcia comstante listaDeteclas e buscando a classe coletive .tecla
+const listaDeTeclas = document.querySelector('.tecla');
 
-function tocaSom(ideElementoAudio) (
+//criaçâo da repetição "white" e trocado pela "for"com variavel,condição e incremento
+for(let contador = 0; contador < listaDeTeclas.clientHeight; contador++){
+    const teclas = listaDeTeclas[contador]; //guarda o cortador da lista
+    const efeito = teclas.classList[1]; //chama 0 2 item classe individual do som
+    const idAudio = `#som"_${efeito}`; //utiliza a junção template string ${ }
+    teclas.onclick = function() {      //função anônima auxiliar para chamar tocaSom
+        tocasom(idAudio);         //chamada da função tocaSom com parÂmetro idAudio
+    }
+    teclas.onkeydown = function(){    
+        teclas.classList.add('ativa');
+    }
 
-    document.querySelector(ideElementoAudio).play();
-    //criando a referencias constante listaDeTeclas e buscando a classe coletiva tecl const listaDeTeclas = document.querySelectorAll('.tecla'); 
-     //ceiação da repetição "while" e trocado pelo "for" com variavel, condição e increm for(let contador = 0; contador < listaDeTeclas.length; contador++) {
-        const teclas listaDeTeclas [contador]; //guarda o contador da lista const efeito teclas.classList[1]; //chama o 2 item classe individual do som const idAudio #som Sefeito); //utiliza a junção template string ${}
-
-tocasom(idAudio):
-
-teclas.onclick = function(){ //função anônima auxiliar para chamar tocasom //chamada da função tocasom com parâmetro idAudio
-
-teclas.onkeydown = function(evento) {
-
-console.log(evento.code == 'Space')
-
-if (evento.code == 'Space')( if(evento.code != 'Tab'){ teclas.classList.add('ativa');
-
-teclas.onkeyup=function(){ teclas.classList.remove('ativa');
+}
